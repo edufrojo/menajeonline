2ef4fb9e8d87918aa951597c603f064d85634165
edufrojo <edufrojo@gmail.com>
2020-11-12 17:04:33 +0100

    feat: crear seccion contacto

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -1,11 +1,17 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Angular Material
 import { MaterialModule } from './../material.module';
 
+// Rutas
 import { RouterModule } from '@angular/router';
+
+// Componentes comunes
 import { SharedModule } from './../shared/shared.module';
 
+// Componentes páginas
 import { PagesComponent } from './pages.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
