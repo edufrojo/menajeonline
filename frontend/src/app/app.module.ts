3508d3e6d265e6ec1cddf276c07ed2c72397e244
edufrojo <edufrojo@gmail.com>
2020-11-12 12:26:35 +0100

    fix: actulizar header, footer y material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './../app/shared/shared.module';
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MaterialModule,
     AppRoutingModule,
     SharedModule,
     PagesModule,
