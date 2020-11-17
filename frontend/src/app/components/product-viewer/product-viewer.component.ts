96d4833f82700088f32a98c8f2424264314fdcea
edufrojo <edufrojo@gmail.com>
2020-11-17 12:03:49 +0100

    feat: crear componente 'product-viewer'

--- frontend/src/app/components/product-viewer/product-viewer.component.ts
+++ frontend/src/app/components/product-viewer/product-viewer.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Articulo } from './../../models/articulo.model';
+import { ArticuloService } from './../../services/articulo.service';
 
 @Component({
   selector: 'app-product-viewer',
   templateUrl: './product-viewer.component.html',
-  styleUrls: ['./product-viewer.component.scss']
+  styleUrls: ['./product-viewer.component.scss'],
 })
 export class ProductViewerComponent implements OnInit {
+  articulos: Articulo[];
+  @Input() public promotion: string;
 
-  constructor() { }
+  constructor(
+    private articuloService: ArticuloService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    console.log(this.promotion);
+    this.getArticulosByPromotion(this.promotion);
   }
 
+  getArticulosByPromotion(promotion) {
+    this.articuloService
+      .getArticulosByPromotion(this.promotion)
+      .subscribe((resp: any) => {
+        this.articulos = resp.articulos;
+      });
+  }
 }
