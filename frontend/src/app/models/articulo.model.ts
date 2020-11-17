e6166012fa279455baebf38570ec4c438d62002b
edufrojo <edufrojo@gmail.com>
2020-11-17 11:58:41 +0100

    feat: crear modelo articulo

---
+++ frontend/src/app/models/articulo.model.ts
@@ -0,0 +1,27 @@
+export interface Articulo {
+  _id: Array<any>;
+  cod_articulo: Array<any>;
+  descripcion: Array<any>;
+  descripcion2: Array<any>;
+  ref_pve: Array<any>;
+  nomenclatura: Array<any>;
+  departamento: Array<any>;
+  familia: Array<any>;
+  categoria: Array<any>;
+  gama: Array<any>;
+  tipo_producto: Array<any>;
+  multiplo_venta: Array<any>;
+  precio_coste: Array<any>;
+  ecotasa: Array<any>;
+  tipo_iva: Array<any>;
+  PVP: Array<any>;
+  unidad_medida: Array<any>;
+  estrategico: Array<any>;
+  venta_internet: Array<any>;
+  surtido_optimus: Array<any>;
+  cod_marca: Array<any>;
+  url_imagen: Array<any>;
+  cod_promocion: Array<any>;
+  fecha_inicio_promocion: Array<any>;
+  fecha_final_promocion: Array<any>;
+}
