93ab35032df3c59cbce482c489a655165ecddd8c
edufrojo <edufrojo@gmail.com>
2020-11-19 09:33:41 +0100

    fix: scroll infinto promotion-detail

--- frontend/src/app/pages/promotion-detail/promotion-detail.component.ts
+++ frontend/src/app/pages/promotion-detail/promotion-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { Articulo } from './../../models/articulo.model';
 import { ArticuloService } from './../../services/articulo.service';
@@ -19,27 +19,48 @@ export class PromotionDetailComponent implements OnInit {
   constructor(
     private promocionService: PromocionService,
     private articuloService: ArticuloService,
+    private router: Router,
     private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.getPromocion();
-    this.getProductsByPromotion(this.promocion);
+    this.loadData();
   }
 
-  getPromocion() {
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const pos =
+      (document.documentElement.scrollTop || document.body.scrollTop) + 2000;
+    const max =
+      document.documentElement.scrollHeight || document.body.scrollHeight;
+
+    if (pos > max) {
+      if (this.articuloService.cargando == false) {
+        this.articuloService
+          .getProductsByPromotion(this.promocion.cod_promocion)
+          .subscribe((resp: any) => {
+            this.articulos.push(...resp.articulos);
+          });
+      }
+    }
+  }
+
+  loadData() {
     const id = this.activatedRoute.snapshot.params.id;
 
     this.promocionService.getPromocionById(id).subscribe((resp: any) => {
       this.promocion = resp.promocion;
+
+      this.articuloService
+        .getProductsByPromotion(this.promocion.cod_promocion)
+        .subscribe((resp: any) => {
+          this.articulos = resp.articulos;
+          console.log(this.articulos);
+        });
     });
   }
 
-  getProductsByPromotion(promotion) {
-    this.articuloService
-      .getProductsByPromotion(promotion)
-      .subscribe((resp: any) => {
-        this.articulos = resp.articulos;
-      });
+  onProductClick(articulo: Articulo) {
+    this.router.navigate(['producto', articulo._id]);
   }
 }
