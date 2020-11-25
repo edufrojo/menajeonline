96d272e4aa16b2da8bcc33c4401dcefbe7177d5e
edufrojo <edufrojo@gmail.com>
2020-11-25 09:05:46 +0100

    feat: crear boton top

---
+++ frontend/src/app/components/boton-top/boton-top.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-boton-top',
+  templateUrl: './boton-top.component.html',
+  styleUrls: ['./boton-top.component.scss'],
+})
+export class BotonTopComponent implements OnInit {
+  windowScrolled: boolean;
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop > 100
+    ) {
+      this.windowScrolled = true;
+    } else if (
+      (this.windowScrolled && window.pageYOffset) ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop < 10
+    ) {
+      this.windowScrolled = false;
+    }
+  }
+  scrollToTop() {
+    (function smoothscroll() {
+      var currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - currentScroll / 8);
+      }
+    })();
+  }
+  ngOnInit() {}
+}
--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -10,10 +10,23 @@ import { SharedModule } from './../shared/shared.module';
 
 // Componentes páginas
 import { ProductViewerComponent } from './product-viewer/product-viewer.component';
+import { WelcomeInfoComponent } from './welcome-info/welcome-info.component';
+import { BotonAtrasComponent } from './boton-atras/boton-atras.component';
+import { BotonTopComponent } from './boton-top/boton-top.component';
 
 @NgModule({
-  declarations: [ProductViewerComponent],
+  declarations: [
+    ProductViewerComponent,
+    WelcomeInfoComponent,
+    BotonAtrasComponent,
+    BotonTopComponent,
+  ],
   imports: [CommonModule, MaterialModule, SharedModule],
-  exports: [ProductViewerComponent],
+  exports: [
+    ProductViewerComponent,
+    WelcomeInfoComponent,
+    BotonAtrasComponent,
+    BotonTopComponent,
+  ],
 })
 export class ComponentsModule {}
